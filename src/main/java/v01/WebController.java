6b86821ad81a0cb878a97864c5babd022cc48260
krzysiek28 <krzysztof.uliasz@gmail.com>
2018-11-17 15:21:49 +0100

    Added spring configuration on maven project.

---
+++ src/main/java/v01/WebController.java
@@ -0,0 +1,18 @@
+package v01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class WebController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/")
+    public String homePage(){
+        return "home";
+    }
+}
